ed47426e27dce174ef8195116b8634c5e26c5bb9
shamsi24 <137806833+shamsi24@users.noreply.github.com>
2026-01-06 12:34:21 +0400

    feat: implement splash page and upgrade alert functionality

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -9,7 +9,8 @@ import 'package:shamsi_mobile_template/app/generic/generic_state.dart';
 import 'package:shamsi_mobile_template/presentation/router/navigation.dart';
 import 'package:shamsi_mobile_template/presentation/router/route_generator.dart';
 import 'package:shamsi_mobile_template/presentation/shared/shared.dart';
-import 'package:shamsi_mobile_template/presentation/ui/main/view/main_page.dart';
+import 'package:shamsi_mobile_template/presentation/ui/splash_page/splash_page.dart';
+import 'package:upgrader/upgrader.dart';
 
 // The main application widget, which is stateless and manages the initial setup.
 class App extends StatelessWidget {
@@ -27,8 +28,10 @@ class App extends StatelessWidget {
         splitScreenMode: true,
         builder: (context, child) {
           return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            navigatorObservers: [Navigation.routeObserver],
             // App title and theme configuration.
-            title: "E-Bina",
+            title: "SHAMSI MOBILE TEMPLATE",
             theme: UITheme.appTheme,
             navigatorKey:
                 Navigation.navigatorKey, // Navigation control via a global key.
@@ -39,15 +42,19 @@ class App extends StatelessWidget {
                 context.localizationDelegates, // Localization delegate.
             onGenerateRoute:
                 RouteGenerator.generateRoute, // Handles route generation.
-            home: GenericConsumer<AppCubit, GenericState>(
-              // Listens to the state changes and updates the UI accordingly.
-              builder: (context, state) => _buildPage(state),
-              listener: (context, state) {
-                // Handles failure state and prints error details in debug mode.
-                if (state is Failure && kDebugMode) {
-                  print(state.error);
-                }
-              },
+            home: UpgradeAlert(
+              showIgnore: false,
+              showLater: false,
+              child: GenericConsumer<AppCubit, GenericState>(
+                // Listens to the state changes and updates the UI accordingly.
+                builder: (context, state) => _buildPage(state),
+                listener: (context, state) {
+                  // Handles failure state and prints error details in debug mode.
+                  if (state is Failure && kDebugMode) {
+                    debugPrint(state.error.toString());
+                  }
+                },
+              ),
             ),
           );
         },
@@ -57,24 +64,31 @@ class App extends StatelessWidget {
 
   // Determines which page to display based on the current application state.
   Widget _buildPage(GenericState state) {
+    // Handle state transitions specifically for Authorized state
+    if (state is Authorized) {
+      // return const HomePage();
+    }
+
+    // Handle the rest of the states
     switch (state.runtimeType) {
-      case const (Authorized):
-        // If the user is authorized, show the main page.
-        return const MainPage();
       case const (Pin):
       // If a PIN is required for confirmation, show the passcode page.
       // return const PasscodePage(passCodeState: PassCodeState.confirm);
       case const (NavigateToPage):
-      // When navigating via a push notification, show the notifications page.
-      // return const NotificationsPage();
+      // if (state is Unauthorized) {
+      //   return const OnBoardingPage();
+      // } else if (state is Pin) {
+      //   return const PasscodePage(passCodeState: PassCodeState.confirm);
+      // }
       case const (Unauthorized):
-        // If the user is unauthorized, show nothing (empty widget).
-        return const SizedBox.shrink();
+      // If the user is unauthorized, show onboarding page
+      // return const OnBoardingPage();
       case const (SplashScreen):
+      // Return the actual splash page when in splash screen state
+      // return const SplashPage();
       default:
-        // Default case or when loading, show the splash screen.
-        // return const SplashPage();
-        return const SizedBox.shrink();
+        // If state is not recognized, show splash screen
+        return const SplashPage();
     }
   }
 }
--- lib/presentation/router/navigation.dart
+++ lib/presentation/router/navigation.dart
@@ -3,25 +3,29 @@ import 'package:flutter/material.dart';
 part 'route_name.dart';
 
 class Navigation {
+  static final RouteObserver<PageRoute> routeObserver =
+      RouteObserver<PageRoute>();
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   static BuildContext get context => navigatorKey.currentState!.context;
 
-  static push(RouteName routeName, {dynamic arguments}) async {
+  static Future<Object?>? push(RouteName routeName, {dynamic arguments}) async {
     return await navigatorKey.currentState?.pushNamed(
       routeName.route,
       arguments: arguments,
     );
   }
 
-  static pushReplacement(RouteName routeName, {dynamic arguments}) async {
+  static Future<Object?>? pushReplacement(RouteName routeName,
+      {dynamic arguments}) async {
     return await navigatorKey.currentState?.pushReplacementNamed(
       routeName.route,
       arguments: arguments,
     );
   }
 
-  static pushNamedAndRemoveUntil(RouteName routeName, {dynamic arguments}) async {
+  static Future<Object?>? pushNamedAndRemoveUntil(RouteName routeName,
+      {dynamic arguments}) async {
     return await navigatorKey.currentState?.pushNamedAndRemoveUntil(
       routeName.route,
       (route) => route.isFirst,
@@ -29,20 +33,34 @@ class Navigation {
     );
   }
 
-  static popToRootAndPushNamed(RouteName routeName, {dynamic arguments}) async {
+  static Future<dynamic> popToRootAndPushNamed(RouteName routeName,
+      {dynamic arguments}) async {
     navigatorKey.currentState?.popUntil((route) => route.isFirst);
     return await pushReplacement(routeName, arguments: arguments);
   }
 
-  static popUntil(RouteName routeName) {
-    return navigatorKey.currentState?.popUntil(ModalRoute.withName(routeName.route));
+  static void popUntil(RouteName routeName) {
+    return navigatorKey.currentState
+        ?.popUntil(ModalRoute.withName(routeName.route));
   }
 
-  static pop<T>({T? result}) {
+  static Future<dynamic> pushAndRemoveUntil(
+    RouteName newRouteName, {
+    required RouteName removeUntil,
+    dynamic arguments,
+  }) async {
+    return await navigatorKey.currentState?.pushNamedAndRemoveUntil(
+      newRouteName.route,
+      (route) => route.settings.name == removeUntil.route,
+      arguments: arguments,
+    );
+  }
+
+  static void pop<T>({T? result}) {
     return navigatorKey.currentState?.pop(result);
   }
 
-  static popToRoot<T>({T? result}) {
+  static void popToRoot<T>({T? result}) {
     return navigatorKey.currentState?.popUntil((route) => route.isFirst);
   }
 
